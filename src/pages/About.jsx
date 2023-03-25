2df472457556f4247c7b91a5b3a2def9a6db497b
KombinatorVit <slonik0775@gmail.com>
2023-03-25 23:20:25 +0200

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const About = () => {
     return (
         <h1>
-            Это приложение создано в качестве обуч. ролика на ULBI TV
-        </h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur commodi dolore earum eos,
+            illum in iusto laboriosam minus natus neque, nobis, placeat quia recusandae sapiente sed tempore.
+            Consequuntur, illum. </h1>
     );
 };
 
